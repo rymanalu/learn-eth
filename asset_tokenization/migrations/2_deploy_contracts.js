a09be1bbe440253dbc5b1b0a46b27c2e7bda6c66
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-11-02 22:16:29 +0700

    123

--- asset_tokenization/migrations/2_deploy_contracts.js
+++ asset_tokenization/migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
-const MyToken = artifacts.require("./MyToken.sol");
+const MyToken = artifacts.require("MyToken");
+const MyTokenSale = artifacts.require("MyTokenSale");
 
 module.exports = async (deployer) => {
+  const addresses = await web3.eth.getAccounts();
+
   await deployer.deploy(MyToken, 1000000);
+  await deployer.deploy(MyTokenSale, 1, addresses[0], MyToken.address);
+
+  const instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, 1000000);
 };
