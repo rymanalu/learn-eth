6dfbe2f60c5305b00c9dc783a1487a2885897ab9
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-11-04 22:53:32 +0700

    133

--- asset_tokenization/truffle-config.js
+++ asset_tokenization/truffle-config.js
@@ -1,4 +1,9 @@
+require('dotenv').config({ path: './.env' });
+
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+const accountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,6 +13,12 @@ module.exports = {
     development: {
       port: 7545,
       host: "127.0.0.1",
+      network_id: 5777
+    },
+    ganache_local: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, 'http://127.0.0.1:7545', accountIndex);
+      },
       network_id: 5777
     }
   },
