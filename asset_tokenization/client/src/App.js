3e075b952d88578df1daac7a614d926d74f771cc
Roni Yusuf Manalu <rymanalu@gmail.com>
2020-11-04 23:05:12 +0700

    134

--- asset_tokenization/client/src/App.js
+++ asset_tokenization/client/src/App.js
@@ -7,7 +7,11 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { loaded: false, kycAddress: '0x123...' };
+  state = {
+    loaded: false,
+    kycAddress: '0x123...',
+    tokenSaleAddress: null
+   };
 
   componentDidMount = async () => {
     try {
@@ -37,7 +41,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ loaded: true });
+      this.setState({ loaded: true, tokenSaleAddress: MyTokenSale.networks[this.networkId].address });
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -77,6 +81,8 @@ class App extends Component {
         <h2>KYC Whitelisting</h2>
         Address to allow: <input type="text" name="kycAddress" value={kycAddress} onChange={this.handleInputChange} />
         <button type="button" onClick={this.handleKycWhitelisting}>Add to Whitelist</button>
+        <h2>Buy Tokens</h2>
+        <p>If you want to buy tokens, send Wei to this address: {this.state.tokenSaleAddress}</p>
       </div>
     );
   }
